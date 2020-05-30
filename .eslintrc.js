e86c308077c3e92c0367772109ac5eb91f3fab20
webmarkyn <web.markyn@gmail.com>
2020-05-30 23:39:19 +0300

    configured project

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "no-restricted-exports": 0,
+    "no-default-export": 0,
+    "react/jsx-props-no-spreading": "off",
+    "react/prop-types": 0,
+    "import/prefer-default-export": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+        mjs: "never",
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "src/"],
+        "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
