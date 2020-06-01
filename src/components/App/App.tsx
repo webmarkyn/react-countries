8a21ac479c11758f8e6a6612dc0233b7818133f6
webmarkyn <web.markyn@gmail.com>
2020-06-01 21:01:05 +0300

    added router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CountriesList from '../CountriesList';
 import './index.css';
 
-const App = () => <CountriesList />;
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact>
+        <CountriesList />
+      </Route>
+      <Route path="/:country" exact>
+        <h1>Country</h1>
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
